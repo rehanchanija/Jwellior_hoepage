25f2d3db78c5d2e88daf09f8aabfe718131d5fc8
rehanchanija <rehanchanija@gmail.com>
2024-12-07 22:54:26 +0530

    shppingcart

--- components/ShoppingCart.js
+++ components/ShoppingCart.js
@@ -11,31 +11,36 @@ const ShoppingCart = () => {
   return (
     <div className="font-poppins ">
       <h1 className="text-center text-2xl ">Shopping cart</h1>
-      <div className="flex justify-between  w-[36rem] font-poppins ">
+      <div className="flex justify-center  w-[52rem]  ">
         <div className="flex ">
           <Image src="/image/cart.png" height={162} width={162} alt="" />
-          <div className="pt-3 px-12">
-            <h3>Aloe Vera</h3>
-            <p>Body wash Lavender</p>
-            <span className="text-xl text-[#AABB18]"> ₹ 500 </span>
-            <span>₹ 999</span>
-            <span className=" text-[#AABB18]">60 % off </span>
+          <div className="pt-3 px-12  ">
+            <h3 className="text-lg">Aloe Vera</h3>
+            <p className="text-lg">Body wash Lavender</p>
+            <p className="pt-1">
+              <span className="text-2xl text-[#AABB18] ">₹ 500</span>
+              <span className="px-2 text-[#949494] text-lg">₹ 999</span>
+              <span className=" text-[#AABB18] font-medium ">60 % off </span>
+            </p>
+            <Button className="pt-4  font-medium text-[#065934] border-none">
+              <Image src="/image/removeicon.png" className="" />
+              Remove
+            </Button>
           </div>
         </div>
 
-        <div className="">
-          <h1 className="text-2xl pt-6 text-[#065934]">₹ 500</h1>
+        <div className="space-x-2 flex items-center">
           <div>
-            <Button onClick={decrement} className="px-4 py-2 rounded shadow ">
+            <Button onClick={decrement} className="px-4 py-4 ">
               -
             </Button>
-            <span>{count}</span>
-            <Button
-              onClick={increment}
-              className="px-4 py-2   rounded shadow  "
-            >
+            <span className="p-2">{count}</span>
+            <Button onClick={increment} className="px-4 py-4    ">
               +
             </Button>
+          </div>
+          <div>
+            <h1 className="text-2xl pl-6 text-[#065934]">₹ 500</h1>
           </div>
         </div>
       </div>
